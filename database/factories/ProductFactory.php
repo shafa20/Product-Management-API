7fb706fba6fc948f7d12c3f2e6d90c607bce31e3
tbd-shafa <shafa@technobd.com>
2025-04-20 16:17:27 +0600

    unit test

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->randomFloat(2, 1, 100),
+            'category_id' => Category::factory(), // Create a category for each product
+        ];
+    }
+}
+
